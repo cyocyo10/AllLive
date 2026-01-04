b6ff2766cb3bfcbb8f1c737d1df6591cbadd3c15
cyocyo10 <cyocyo012310@gmail.com>
2026-01-04 21:34:36 +0800

    feat: 历史记录添加导出导入功能 - 支持导出为格式化JSON - 支持从JSON导入 - 收藏导出也改为格式化JSON

--- AllLive.UWP/ViewModels/HistoryVM.cs
+++ AllLive.UWP/ViewModels/HistoryVM.cs
@@ -7,6 +7,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+using Newtonsoft.Json;
 
 namespace AllLive.UWP.ViewModels
 {
@@ -16,8 +19,12 @@ namespace AllLive.UWP.ViewModels
         {
             Items = new ObservableCollection<HistoryItem>();
             CleanCommand = new RelayCommand(Clean);
+            InputCommand = new RelayCommand(Input);
+            OutputCommand = new RelayCommand(Output);
         }
         public ICommand CleanCommand { get; set; }
+        public ICommand InputCommand { get; set; }
+        public ICommand OutputCommand { get; set; }
 
         public ObservableCollection<HistoryItem> Items { get; set; }
 
@@ -61,13 +68,12 @@ namespace AllLive.UWP.ViewModels
             {
                 HandleError(ex);
             }
-
         }
+
         public async void Clean()
         {
             try
             {
-
                 var result = await Utils.ShowDialog("清空记录", $"确定要清除全部观看记录吗?");
                 if (!result)
                 {
@@ -82,8 +88,139 @@ namespace AllLive.UWP.ViewModels
             {
                 HandleError(ex);
             }
-
         }
 
+        public async void Input()
+        {
+            FileOpenPicker picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add(".json");
+            picker.SuggestedStartLocation = PickerLocationId.Desktop;
+            picker.ViewMode = PickerViewMode.List;
+            picker.CommitButtonText = "导入";
+
+            var file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                try
+                {
+                    var json = await FileIO.ReadTextAsync(file);
+                    var items = JsonConvert.DeserializeObject<List<HistoryJsonItem>>(json);
+                    foreach (var item in items)
+                    {
+                        DatabaseHelper.AddHistory(new HistoryItem()
+                        {
+                            SiteName = item.SiteName,
+                            RoomID = item.RoomId,
+                            UserName = item.UserName,
+                            Photo = item.Face,
+                            WatchTime = DateTime.TryParse(item.WatchTime, out var dt) ? dt : DateTime.Now
+                        });
+                    }
+                    Utils.ShowMessageToast("导入成功");
+                    Refresh();
+                }
+                catch (Exception ex)
+                {
+                    HandleError(ex);
+                    Utils.ShowMessageToast("导入失败");
+                }
+            }
+        }
+
+        public async void Output()
+        {
+            FileSavePicker picker = new FileSavePicker();
+            picker.FileTypeChoices.Add("Json", new List<string>() { ".json" });
+            picker.SuggestedStartLocation = PickerLocationId.Desktop;
+            picker.SuggestedFileName = "history.json";
+
+            var file = await picker.PickSaveFileAsync();
+            if (file != null)
+            {
+                try
+                {
+                    var items = new List<HistoryJsonItem>();
+                    foreach (var item in Items)
+                    {
+                        var siteId = "";
+                        switch (item.SiteName)
+                        {
+                            case "哔哩哔哩直播":
+                                siteId = "bilibili";
+                                break;
+                            case "斗鱼直播":
+                                siteId = "douyu";
+                                break;
+                            case "虎牙直播":
+                                siteId = "huya";
+                                break;
+                            case "抖音直播":
+                                siteId = "douyin";
+                                break;
+                        }
+
+                        items.Add(new HistoryJsonItem()
+                        {
+                            SiteId = siteId,
+                            Id = $"{siteId}_{item.RoomID}",
+                            RoomId = item.RoomID,
+                            UserName = item.UserName,
+                            Face = item.Photo,
+                            WatchTime = item.WatchTime.ToString("yyyy-MM-dd HH:mm:ss")
+                        });
+                    }
+                    var json = JsonConvert.SerializeObject(items, Formatting.Indented);
+                    await FileIO.WriteTextAsync(file, json);
+                    Utils.ShowMessageToast("导出成功");
+                }
+                catch (Exception ex)
+                {
+                    HandleError(ex);
+                    Utils.ShowMessageToast("导出失败");
+                }
+            }
+        }
+    }
+
+    public class HistoryJsonItem
+    {
+        [JsonProperty("siteId")]
+        public string SiteId;
+
+        [JsonProperty("id")]
+        public string Id;
+
+        [JsonProperty("roomId")]
+        public string RoomId;
+
+        [JsonProperty("userName")]
+        public string UserName;
+
+        [JsonProperty("face")]
+        public string Face;
+
+        [JsonProperty("watchTime")]
+        public string WatchTime;
+
+        [JsonIgnore]
+        public string SiteName
+        {
+            get
+            {
+                switch (SiteId)
+                {
+                    case "bilibili":
+                        return "哔哩哔哩直播";
+                    case "douyu":
+                        return "斗鱼直播";
+                    case "huya":
+                        return "虎牙直播";
+                    case "douyin":
+                        return "抖音直播";
+                    default:
+                        return "未知";
+                }
+            }
+        }
     }
 }
